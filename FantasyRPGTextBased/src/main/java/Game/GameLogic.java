31ec2fc10e642ed4672690338b6fb6fb0ecb5c37
Greig Kerr <greigkerr@virginmedia.com>
2022-09-28 22:46:48 +0100

    User input working and tested

--- FantasyRPGTextBased/src/main/java/Game/GameLogic.java
+++ FantasyRPGTextBased/src/main/java/Game/GameLogic.java
@@ -20,4 +20,28 @@ public class GameLogic {
         return input;
     }
 
+    //method to simulate clearing out the console
+    public static void clearConsole(){
+        for(int i = 0; i < 100; i++)
+            System.out.println();
+    }
+
+    //method to print a seperator with length n
+    public static void printSeperator(int n){
+        for(int i = 0; i < n; i ++)
+            System.out.println("-");
+    }
+    // method to print a heading
+    public static void printHeading(String title){
+        printSeperator(30);
+        System.out.println(title);
+        printSeperator(30);
+    }
+
+    //method to stop the game until user enters anything
+    public static void anythingToContinue(){
+        System.out.println("Enter anything to continue...");
+        scanner.next();
+    }
+
 }
--- FantasyRPGTextBased/src/main/java/Game/Main.java
+++ FantasyRPGTextBased/src/main/java/Game/Main.java
@@ -1,4 +1,13 @@
 package Game;
 
 public class Main {
+
+    public static void main(String[] args){
+
+//        GameLogic.printHeading("Testing helper methods!");
+//        GameLogic.anythingToContinue();
+//        GameLogic.clearConsole();
+//        int input = GameLogic.readInt("Enter 1, 2 or 3: ", 3);
+//        System.out.println("You chose number " + input);
+    }
 }
