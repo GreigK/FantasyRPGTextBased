539f95aa1654f797dfb5309ab1694e7468486193
Greig Kerr <greigkerr@virginmedia.com>
2022-09-28 22:35:09 +0100

    Function to allow user input

--- FantasyRPGTextBased/src/main/java/Game/GameLogic.java
+++ FantasyRPGTextBased/src/main/java/Game/GameLogic.java
@@ -1,4 +1,23 @@
 package Game;
+import java.util.Scanner;
 
 public class GameLogic {
+    static Scanner scanner = new Scanner(System.in);
+
+    // method to get use input from console
+    public static int readInt(String prompt, int userChoices){
+        int input;
+        do{
+            System.out.println(prompt);
+            //try everything within the try and if an error occurs, catch it and do what is inside the catch
+            try{
+                input = Integer.parseInt(scanner.next());
+            }catch(Exception e){
+                input = -1;
+                System.out.println("Please enter an integer!");
+            }
+        }while(input < 1 || input > userChoices);
+        return input;
+    }
+
 }
